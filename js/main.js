7b6bac9fb374a40c5272b21550d105604e8ffb73
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2023-05-17 18:02:20 +0600

    fix  main.js

--- js/main.js
+++ js/main.js
@@ -62,42 +62,4 @@
         this.scrollY > 30 ? headerContainer.classList.add("sticky") : headerContainer.classList.remove("sticky");
     }
 
-    // Switch Button
-
-    let checkbox = document.querySelector('.switch-button-checkbox');
-
-    checkbox.addEventListener('change', e => {
-
-        if(!e.target.checked){
-            var switchBtn = document.querySelector('.switch-button');
-            switchBtn.classList.toggle("on");
-        }
-
-    });
-
-    // Range Slider
-
-    const slideValue = document.querySelector(".range-value .right");
-    const rangeBg = document.querySelector(".range-bg");
-
-    if(slideValue != null || rangeBg !=null){
-        let minValue = 3000;
-        let currentValue = inputSlider.value - minValue;
-        let maxValue = 10000;
-        let rangeValue = (maxValue - minValue);
- 
-        rangeBg.style.width = (((currentValue * 100 ) / rangeValue) - 2.5 ) + '%';
- 
-    }
-
-
-    const filter = document.getElementsByClassName('filter-label');
-
-    for(i=0;i<filter.length;i++){
-        filter[i].addEventListener('click',function(){
-            this.classList.toggle('active');
-        })
-    }
-   
-
 })()
